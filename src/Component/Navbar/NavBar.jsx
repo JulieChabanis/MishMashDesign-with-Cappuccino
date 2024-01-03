105ae1be49c5c353fefc61e0a1043f013215aadc
JulieChabanis <julie.chabanis07@gmail.com>
2024-01-03 14:16:32 +0100

    add coding project link in navbar

--- src/Component/Navbar/NavBar.jsx
+++ src/Component/Navbar/NavBar.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 import styles from '../Navbar/Navbar.module.css'
 
 const NavBar = () => {
+  const devPortfolio = 'https://juliechabanis-portfolio.netlify.app/'
   return (
-    <div className={styles.navbarContainer}>
-      <h1 className={styles.title}>A Michmach of illustrations • By Julie Chabanis & Léon the Pug</h1>
-    </div>
+    <>
+      <div className={styles.navbarContainer}>
+        <h1 className={styles.title}>A Michmach of illustrations • By Julie Chabanis & Léon the Pug</h1>
+      </div>
+      <div className={styles.ElementsNavbar}>
+      <h2 className={styles.subtitle}>Go to</h2>
+      <a href={devPortfolio} className={styles.link}>my coding projects</a>
+      </div>
+    </>
   )
 }
 
